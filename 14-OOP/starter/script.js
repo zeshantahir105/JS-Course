6e73833cf9c70288e4d3117bce6df8dc6a827431
zeshantahir105 <zeshantahir105@gmail.com>
2022-07-22 12:17:52 +0500

    Working on Mapty

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -9,7 +9,7 @@ const Person = function (firstName, birthYear) {
   //   Dont add functions in the constructor functions
 };
 const jonas = new Person('Jonas', 1992);
-console.log(jonas);
+// console.log(jonas);
 // 1. New {} is created.
 // 2. Function is called, this = {}
 // 3. {} linked to prototype.
@@ -17,66 +17,66 @@ console.log(jonas);
 
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('jack', 1975);
-console.log(matilda, jack);
-console.log(jonas instanceof Person); // True
+// console.log(matilda, jack);
+// console.log(jonas instanceof Person); // True
 
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
 // Prototypes
 Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
+  // console.log(2037 - this.birthYear);
 };
 
 jonas.calcAge();
 matilda.calcAge();
-console.log(jonas.birthYear);
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
-console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(jonas.birthYear);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(jonas));
 
 Person.prototype.species = 'My Species';
-console.log(jonas.species);
-console.log(jonas.hasOwnProperty('species'));
+// console.log(jonas.species);
+// console.log(jonas.hasOwnProperty('species'));
 
-console.log(jonas.__proto__.__proto__);
-console.log(Person.prototype.constructor);
-console.log('asdasdasd');
+// console.log(jonas.__proto__.__proto__);
+// console.log(Person.prototype.constructor);
+// console.log('asdasdasd');
 console.dir(Person.prototype.constructor);
 const arr = [3, 6, 4, 5, 6, 9, 3];
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
 
 Array.prototype.unique = function () {
   return [...new Set(this)];
 };
 
-console.log(arr.unique());
+// console.log(arr.unique());
 const h1 = document.querySelector('h1');
-console.log(h1.__proto__);
+// console.log(h1.__proto__);
 
-// Challenge 1
-const car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// // Challenge 1
+// const car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log('Accelerated! : ', this.speed);
-};
-car.prototype.brake = function () {
-  this.speed -= 10;
-  console.log('Applied Break! : ', this.speed);
-};
+// car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   // console.log('Accelerated! : ', this.speed);
+// };
+// car.prototype.brake = function () {
+//   this.speed -= 10;
+//   // console.log('Applied Break! : ', this.speed);
+// };
 
-const C = new car(1, 40);
-C.accelerate();
-C.brake();
-C.brake();
+// const C = new car(1, 40);
+// C.accelerate();
+// C.brake();
+// C.brake();
 
 // ES6 Classes ( Using Class Expression )
 
-console.log('------------------------------');
+// console.log('------------------------------');
 // const PersonCl = class {};
 class PersonCl {
   constructor(firstName, birthYear) {
@@ -85,15 +85,15 @@ class PersonCl {
   }
 
   calcAge() {
-    console.log('Khud nikalo boss 🤔');
+    // console.log('Khud nikalo boss 🤔');
   }
 
   get Nickname() {
-    console.log('ULUU');
+    // console.log('ULUU');
   }
   // Setting
   set fullName(name) {
-    console.log(name);
+    // console.log(name);
     if (name.includes(' ')) this._fullName = name;
     else alert('NOT A FULL NAME');
   }
@@ -104,37 +104,122 @@ class PersonCl {
 }
 
 const Umair = new PersonCl('Umair Qaiser', 1999);
-console.log(Umair);
+// console.log(Umair);
 // Umair.calcAge();
 // Umair.Nickna
 // Umair.fullName;
 
 // CHALLENGE 2
 
-class Car {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
 
-  accelerate() {
-    this.speed += 10;
-  }
+//   accelerate() {
+//     this.speed += 10;
+//   }
 
-  break() {
-    this.speed -= 10;
-  }
+//   break() {
+//     this.speed -= 10;
+//   }
 
-  set speedUS(speed) {
-    this.speed = speed * 1.6;
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+//   get speedUS() {
+//     return this.speed / 16;
+//   }
+// }
+
+// const x = new Car('X', 35);
+// x.accelerate();
+// x.speedUS = 50;
+// // console.log(x);
+// // console.log('Pakistan');
+
+// Inheritance between Constructor Functions
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  this.course = course;
+};
+
+const zesh = new Student('Zeshan', 2022, 'CE');
+// Linking the prototypes.
+// Student.prototype = Object.create(Person.prototype); // Emopty Object
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+console.log(zesh);
+zesh.introduce();
+
+// Coding Challenge #3
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+};
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= -1;
+  console.log(
+    `${this.make} going at ${this.speed}km/h, with a charge of ${this.charge}`
+  );
+};
+const Honda = new EV('Honda', 150, 10);
+Honda.accelerate();
+Honda.accelerate();
+Honda.accelerate();
+Honda.brake();
+Honda.accelerate();
+Honda.accelerate();
+Honda.accelerate();
+Honda.accelerate();
+Honda.chargeBattery(90);
+Honda.accelerate();
+
+// Inheritance between classes.
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
   }
-  get speedUS() {
-    return this.speed / 16;
+  introduce() {
+    console.log('Introduce');
   }
 }
 
-const x = new Car('X', 35);
-x.accelerate();
-x.speedUS = 50;
-console.log(x);
-a;
+const hamza = new StudentCl('M. Hamza', 2018, 'CE');
+hamza.introduce();
+hamza.calcAge();
+
+// Challenge # 04
+
+// class EVCL extends CarCl {
+//   #charge;
+//   constructor(make, speed, charge) {
+//     super(make, speed);
+//     this.charge = charge;
+//   }
+// }
--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,116 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // kn/h
+    this.calcSpeed = this.distance / (this.duration / 60);
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cyc1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cyc1);
+
+class App {
+  #map;
+  // #mapEvent;
+  #coords;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
+  }
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      function () {
+        // ERROR SCNERIO
+        alert("Couldn't get your position!");
+      }
+    );
+  }
+  _loadMap(position) {
+    // SUCCESS SCNERIO
+    const { latitude, longitude } = position.coords;
+    // const { longitude } = position.coords;
+    // console.log(position);
+    // console.log(latitude, longitude);
+
+    this.#coords = [latitude, longitude];
+    this.#map = L.map('map').setView(this.#coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Handling Clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+  _showForm(mapE) {
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    // Display Marker
+    L.marker(this.#coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
+
+const app = new App();
+// app._getPosition();
