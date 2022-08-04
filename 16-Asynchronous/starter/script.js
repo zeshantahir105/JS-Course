9c9c49b4e9accf7b40edf6ed4dd7d64415e79f83
zeshantahir105 <zeshantahir105@gmail.com>
2022-08-04 10:34:16 +0500

    Async Js DONE

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,6 +1,40 @@
 'use strict';
 
+console.log('Zinda hai!');
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  console.log(request.responseText);
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+    let data = JSON.parse(this.responseText);
+    data = data[0];
+    console.log(data);
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h 4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+    </article>
+        `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+// Asynchronous JavaScript
+getCountryData('Pakistan');
+getCountryData('China');
+getCountryData('Germany');
